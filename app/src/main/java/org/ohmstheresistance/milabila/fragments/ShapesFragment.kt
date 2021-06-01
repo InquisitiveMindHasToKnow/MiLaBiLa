a5a88eb5fdcd6ef7dc9c0d742ded0b322be3ba40
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2021-06-01 03:35:53 -0400

    ShapesFragment modified with recyclerview, textview and imageviews that update on interaction

--- app/src/main/java/org/ohmstheresistance/milabila/fragments/ShapesFragment.kt
+++ app/src/main/java/org/ohmstheresistance/milabila/fragments/ShapesFragment.kt
@@ -6,17 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import org.ohmstheresistance.milabila.R
 import org.ohmstheresistance.milabila.databinding.ShapesFragmentBinding
+import org.ohmstheresistance.milabila.dataclasses.ShapeData
+import org.ohmstheresistance.milabila.recyclerview.ShapesAdapter
 
-class ShapesFragment : Fragment() {
+private val shapesList = listOf(
+    ShapeData(R.drawable.square, "Square"),
+    ShapeData(R.drawable.circle_shape, "Circle"),
+    ShapeData(R.drawable.rectangle, "Rectangle"),
+    ShapeData(R.drawable.cube, "Cube"),
+    ShapeData(R.drawable.heart, "Heart"),
+    ShapeData(R.drawable.diamond, "Diamond"),
+    ShapeData(R.drawable.hexagon, "Hexagon"),
+    ShapeData(R.drawable.pentagon, "Pentagon"),
+    ShapeData(R.drawable.octogon, "Octagon"),
+    ShapeData(R.drawable.triangle, "Triangle"))
+
+
+
+class ShapesFragment : Fragment(), ShapesAdapter.ShapesViewHolder.UpdateShapeImageAndNameInterface {
+
+    lateinit var shapesFragmentBinding: ShapesFragmentBinding
+    private val shapesAdapter = ShapesAdapter(shapesList, this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       val shapesFragmentBinding = DataBindingUtil.inflate<ShapesFragmentBinding>(inflater, R.layout.shapes_fragment, container, false)
+       shapesFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.shapes_fragment, container, false)
+
+        setUpLettersRecyclerView()
 
         return shapesFragmentBinding.root
+    }
+
+    private fun setUpLettersRecyclerView() {
+        shapesFragmentBinding.shapesRecycler.layoutManager = GridLayoutManager(context, 5)
+        shapesFragmentBinding.shapesRecycler.adapter = shapesAdapter
+    }
+
+    override fun updateShapeImageAndName(clickedShapeName: String, clickedShapeImage: Int) {
+        shapesFragmentBinding.shapefragShapeNameTextview.text = clickedShapeName
+
+        for (i in shapesList.indices){
+            shapesFragmentBinding.shapesfragShapeImageview.setImageResource(clickedShapeImage)
+        }
     }
 }
