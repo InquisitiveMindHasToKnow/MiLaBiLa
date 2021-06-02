19df99a8a45cb1625d286c8373402dfd4a1336b2
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2021-06-01 21:48:41 -0400

    Drawables added for all numbers and their counting amounts. NumbersFragment layout modified to display RV and images of numbers and their amounts.

--- app/src/main/java/org/ohmstheresistance/milabila/fragments/NumbersFragment.kt
+++ app/src/main/java/org/ohmstheresistance/milabila/fragments/NumbersFragment.kt
@@ -6,15 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import org.ohmstheresistance.milabila.R
 import org.ohmstheresistance.milabila.databinding.NumbersFragmentBinding
+import org.ohmstheresistance.milabila.dataclasses.NumbersData
+import org.ohmstheresistance.milabila.recyclerview.NumbersAdapter
 
-class NumbersFragment : Fragment() {
+class NumbersFragment : Fragment(), NumbersAdapter.UpdateNumberDetailsInterface {
+
+    private val numberList = listOf(
+        NumbersData("One", R.drawable.number_one, R.drawable.one_count),
+        NumbersData("Two", R.drawable.nunber_two, R.drawable.two_count),
+        NumbersData("Three", R.drawable.number_three, R.drawable.three_count),
+        NumbersData("Four", R.drawable.number_four, R.drawable.four_count),
+        NumbersData("Five", R.drawable.number_five, R.drawable.five_count),
+        NumbersData("Six", R.drawable.number_six, R.drawable.six_count),
+        NumbersData("Seven", R.drawable.number_seven, R.drawable.seven_count),
+        NumbersData("Eight", R.drawable.number_eight, R.drawable.eight_count),
+        NumbersData("Nine", R.drawable.number_nine, R.drawable.nine_count),
+        NumbersData("Ten", R.drawable.number_ten, R.drawable.ten_count))
+
+    lateinit var numbersFragmentBinding: NumbersFragmentBinding
+    private val numbersAdapter = NumbersAdapter(numberList, this)
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val numbersFragmentBinding = DataBindingUtil.inflate<NumbersFragmentBinding>(inflater, R.layout.numbers_fragment, container, false)
+         numbersFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.numbers_fragment, container, false)
+
+        setUpNumbersRecyclerView()
 
         return numbersFragmentBinding.root
+    }
+
+    private fun setUpNumbersRecyclerView() {
+        numbersFragmentBinding.numberListRecycler.layoutManager = GridLayoutManager(context, 5)
+        numbersFragmentBinding.numberListRecycler.adapter = numbersAdapter
+    }
+
+    override fun updateNumberTextAndImages(numberName: String, numberImage: Int, numberCountImage: Int) {
+        numbersFragmentBinding.theNumbersFragmentHeaderTextview.text = numberName
+        numbersFragmentBinding.selectedNumberImageview.setImageResource(numberImage)
+        numbersFragmentBinding.numberComparisonImageview.setImageResource(numberCountImage)
+
+        when(numberName){
+            "One" -> {numbersFragmentBinding.numberDetailTextview.text = "There is" + numberName + " finger."}
+            "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" -> {numbersFragmentBinding.numberDetailTextview.text = "There are " + numberName + " fingers."}
+        }
     }
 }
---
+++ app/src/main/java/org/ohmstheresistance/milabila/recyclerview/NumbersAdapter.kt
@@ -0,0 +1,49 @@
+package org.ohmstheresistance.milabila.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import org.ohmstheresistance.milabila.R
+import org.ohmstheresistance.milabila.dataclasses.NumbersData
+import org.ohmstheresistance.milabila.fragments.NumbersFragment
+
+class NumbersAdapter(private val numberList: List<NumbersData>, private val numbersFragment: NumbersFragment):
+    RecyclerView.Adapter<NumbersAdapter.NumbersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.number_itemview, parent, false)
+
+        return NumbersViewHolder(v)
+    }
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.bindItems(numberList[position])
+
+        val numberClicked = numberList[position].numberName
+        val numberClickedImage = numberList[position].numberImage
+        val numberClickedCount = numberList[position].numberCountImage
+
+        holder.itemView.setOnClickListener {
+            numbersFragment.updateNumberTextAndImages(
+                numberClicked,
+                numberClickedImage,
+                numberClickedCount
+            )
+        }
+    }
+    override fun getItemCount(): Int {
+        return numberList.size
+    }
+
+    class NumbersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(numbersData: NumbersData) {
+            val letterImage: ImageView = itemView.findViewById(R.id.number_imageview) as ImageView
+            letterImage.setImageResource(numbersData.numberImage)
+
+        }
+    }
+    interface UpdateNumberDetailsInterface {
+        fun updateNumberTextAndImages(numberName: String, numberImage: Int, numberCountImage: Int)
+    }
+}
