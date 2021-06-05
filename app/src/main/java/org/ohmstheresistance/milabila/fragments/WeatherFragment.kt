fecbe8b26e5c4c52d299e4eec822b7588138a372
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2021-06-05 11:35:08 -0400

    Added text to speech feature to weather fragment.

--- app/src/main/java/org/ohmstheresistance/milabila/fragments/WeatherFragment.kt
+++ app/src/main/java/org/ohmstheresistance/milabila/fragments/WeatherFragment.kt
@@ -1,18 +1,21 @@
 package org.ohmstheresistance.milabila.fragments
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import org.ohmstheresistance.milabila.R
 import org.ohmstheresistance.milabila.databinding.WeatherFragmentBinding
 import org.ohmstheresistance.milabila.dataclasses.WeatherData
 import org.ohmstheresistance.milabila.recyclerview.WeatherAdapter
+import java.util.*
 
-class WeatherFragment : Fragment() {
+class WeatherFragment : Fragment(),WeatherAdapter.PassWeatherNameInterface,  TextToSpeech.OnInitListener {
 
     private val weatherList = listOf(
         WeatherData("Sunny", R.drawable.sunny, R.drawable.sun_icon),
@@ -20,10 +23,14 @@ class WeatherFragment : Fragment() {
         WeatherData("Cloudy", R.drawable.cloudy, R.drawable.cloud),
         WeatherData("Foggy", R.drawable.alt_foggy, R.drawable.fog_icon),
         WeatherData("Snowy", R.drawable.snowing, R.drawable.snow_icon),
-        WeatherData("Windy", R.drawable.alt_windy, R.drawable.windy_icon)
+        WeatherData("Windy", R.drawable.alt_windy, R.drawable.windy_icon),
+        WeatherData("Lightning", R.drawable.lightning, R.drawable.lightning_icon),
+        WeatherData("Tornado", R.drawable.tornado, R.drawable.tornado_icon)
     )
+
     private val weatherAdapter = WeatherAdapter(weatherList, this)
     lateinit var weatherFragmentBinding: WeatherFragmentBinding
+    private lateinit var textToSpeech: TextToSpeech
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +39,8 @@ class WeatherFragment : Fragment() {
         weatherFragmentBinding =
             DataBindingUtil.inflate(inflater, R.layout.weather_fragment, container, false)
 
+        textToSpeech = TextToSpeech(context, this)
+
         setupWeatherRecyclerView()
 
         return weatherFragmentBinding.root
@@ -39,7 +48,42 @@ class WeatherFragment : Fragment() {
 
     private fun setupWeatherRecyclerView() {
 
-        weatherFragmentBinding.weatherRecycler.layoutManager = GridLayoutManager(context, 3)
+        weatherFragmentBinding.weatherRecycler.layoutManager = GridLayoutManager(activity, 3)
         weatherFragmentBinding.weatherRecycler.adapter = weatherAdapter
+    }
+
+    override fun onInit(textToSpeechInitialized: Int) {
+        if (textToSpeechInitialized == TextToSpeech.SUCCESS) {
+            val res: Int = textToSpeech.setLanguage(Locale.ENGLISH)
+            if (res == TextToSpeech.LANG_MISSING_DATA || res == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(context, "Unsupported language.", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(context, "Text to speech initialization failed.", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (textToSpeech != null) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+    }
+
+    override fun passWeatherNameForTTS(clickedWeatherName: String) {
+        var sentenceStartingPhrase = ""
+
+         if(clickedWeatherName.equals("Sunny")||clickedWeatherName.equals("Windy")){
+
+            sentenceStartingPhrase = "It is "
+
+        } else {
+            sentenceStartingPhrase = "This is a "
+        }
+
+        val sentenceToSay = sentenceStartingPhrase + clickedWeatherName
+        textToSpeech.speak(sentenceToSay, TextToSpeech.QUEUE_FLUSH, null)
     }
 }
